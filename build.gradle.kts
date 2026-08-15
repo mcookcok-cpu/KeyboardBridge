27699fad0f2f2135914613be428bdbcc08fc9056
root <root@julianto>
2026-08-15 13:06:56 +0700

    Add mavenCentral repository to allprojects to resolve bouncycastle dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
